e5acdba2059318145a31a465ef537d20f4db4ada
Rafael Faria <rafaelsafaria@gmail.com>
2020-04-26 01:57:31 -0300

    changed readme, gitignore, deleted nowignore

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
-
 import App from './App';
 
 ReactDOM.render(
-<React.StrictMode>
-  <App />
-</React.StrictMode>, document.getElementById("root"));
-
-serviceWorker.unregister();
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
